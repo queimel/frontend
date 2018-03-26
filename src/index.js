e436ae05dafa64bd77607512d3bcf61944883197
Cristian Campos <cristian.campos@globant.com>
2018-03-26 12:56:22 -0300

    Agrega live reload y sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-
+import '../src/scss/styles.scss';
 var _ = require('lodash');
+
+
 
 var servicios = require("./servicios");
 servicios.setValor(servicios.getSuma(16.5, 20));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+var HTMLWebpackPlugin = require("html-webpack-plugin");
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve("src/index.html"),
+    }),
+    new LiveReloadPlugin({})
+  ],
+  module:{
+    rules:[
+            {
+                test:/\.(s*)css$/,
+                use:['style-loader','css-loader', 'sass-loader']
+             }
+     ]
+  },
 };
